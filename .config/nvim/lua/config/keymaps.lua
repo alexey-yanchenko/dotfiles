58297bff7cd8984299abc0a0c294f1228eca63d6
Alexey Yanchenko <your.elkin@gmail.com>
2024-08-17 11:37:36 +0700

    Update which-key config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<Tab>", "<Cmd>tabnext<CR>", { desc = "Next Tab" })
 vim.keymap.set("n", "<S-Tab>", "<Cmd>tabprevious<CR>", { desc = "Previous Tab" })
 vim.keymap.set("n", "<leader>h", "<Cmd>HopWord<CR>", { desc = "Easy motion"})
-vim.keymap.set("n", "<leader>b", "<Cmd>BlameToggle<CR>", { desc = "Blame git"})
+vim.keymap.set("n", "<leader>gb", "<Cmd>BlameToggle<CR>", { desc = "Blame git"})
 vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>")
 vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
 vim.keymap.set("n", "<leader>m", "<Cmd>MCstart<cr>", { desc = "Multicursor"})
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,34 +5,32 @@ return {
     event = "VeryLazy",
     opts = {
       plugins = { spelling = true },
-      defaults = {
-        mode = { "n", "v" },
-        ["g"] = { name = "+goto" },
-        ["gs"] = { name = "+surround" },
-        ["]"] = { name = "+next" },
-        ["["] = { name = "+prev" },
-        ["<leader><tab>"] = { name = "+tabs" },
-        ["<leader>b"] = { name = "+buffer" },
-        ["<leader>c"] = { name = "+code" },
-        ["<leader>f"] = { name = "+file/find" },
-        -- ["<leader>g"] = { name = "+git" },
-        ["<leader>gh"] = { name = "+hunks" },
-        ["<leader>q"] = { name = "+quit/session" },
-        ["<leader>s"] = { name = "+search" },
-        ["<leader>u"] = { name = "+ui" },
-        ["<leader>w"] = { name = "+windows" },
-        ["<leader>x"] = { name = "+diagnostics/quickfix" },
-      },
-			window = {
+			win = {
 				border = { '', '▔', '', '', '', ' ', '', '' },
-				margin = { 0, 0, 0, 0 },
 				padding = { 2, 2, 2, 2 },
 			},
     },
     config = function(_, opts)
       local wk = require("which-key")
       wk.setup(opts)
-      wk.register(opts.defaults)
+      wk.add(
+         {
+           mode = { "n", "v" },
+           { "<leader><tab>", group = "tabs" },
+           { "<leader>c", group = "code" },
+           { "<leader>d", group = "LSP" },
+           { "<leader>g", group = "GIT" },
+           { "<leader>f", group = "file/find" },
+           { "<leader>q", group = "quit/session" },
+           { "<leader>s", group = "search" },
+           { "<leader>u", group = "ui" },
+           { "<leader>w", group = "windows" },
+           { "[", group = "prev" },
+           { "]", group = "next" },
+           { "g", group = "goto" },
+           { "gs", group = "surround" },
+         }
+       )
     end,
   },
 }
