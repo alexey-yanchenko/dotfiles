63db4d38481df43ceac2ab40470e4636bd9dccd0
Alexey Yanchenko <your.elkin@gmail.com>
2025-09-27 20:20:52 +0700

    Nvim: fix nordic error

--- .config/nvim/lua/plugins/nordic.lua
+++ .config/nvim/lua/plugins/nordic.lua
@@ -4,21 +4,21 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-			local C = require 'nordic.colors'
+      local C = require 'nordic.colors'
       require('nordic').setup {
         bright_border = true,
-				telescope = {
-					style = 'classic',
-				},
-				override = {
-					WhichKeyBorder = { fg = C.white0, bg = C.gray1 },
-					WhichKeyFloat = { bg = C.gray1 },
-          Visual = { bg = "#3A515D", bold = true, },
-          CmpDocumentation = { fg = C.white0, bg = C.gray1 },
-          CmpDocumentationBorder = { fg = C.white0, bg = '#3A515D' },
-          CmpGhostText = { fg = C.white0, bg = C.gray1 },
-				}
-			}
+        telescope = {
+          style = 'classic',
+        },
+        on_highlight = function(highlights, palette)
+          highlights.WhichKeyBorder = { fg = C.white0, bg = C.gray1 }
+          highlights.WhichKeyFloat = { bg = C.gray1 }
+          highlights.Visual = { bg = "#3A515D", bold = true, }
+          highlights.CmpDocumentation = { fg = C.white0, bg = C.gray1 }
+          highlights.CmpDocumentationBorder = { fg = C.white0, bg = '#3A515D' }
+          highlights.CmpGhostText = { fg = C.white0, bg = C.gray1 }
+        end,
+      }
     end
   }
 }
