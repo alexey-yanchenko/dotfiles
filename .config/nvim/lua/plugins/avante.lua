7fc5bc3b3018af384eb990bc6aaee6c890696563
Alexey Yanchenko <your.elkin@gmail.com>
2025-12-01 16:33:36 +0700

    Nvim: avante cfg

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,30 +1,27 @@
 return {
   {
     "yetone/avante.nvim",
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
     build = "make",
     event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
+    version = false,
     opts = {
-      -- add any opts here
-      -- this file can contain specific instructions for your project
       instructions_file = "avante.md",
-      -- for example
-      -- mode = "legacy",
-      provider = "openrouter",
+      provider = "routerai",
       providers = {
-        openrouter = {
+        routerai = {
           __inherited_from = "openai",
           endpoint = "https://routerai.ru/api/v1/",
-          model = "anthropic/claude-sonnet-4.5",
-          disabled_tools = { "python" },
-          model_names = {
+          model = "anthropic/claude-sonnet-4.5", -- Модель по умолчанию
+          disabled_tools = { "python" }, -- Здесь можно указать какие инструменты Avante не будет использовать
+          model_names = { -- Список моделей доступных для выбора
             "x-ai/grok-code-fast-1",
+            "x-ai/grok-4.1-fast",
+            "qwen/qwen3-coder-plus",
             "anthropic/claude-sonnet-4.5",
+            "anthropic/claude-haiku-4.5",
           },
           api_key_name = 'ROUTERAI_API_KEY',
-          timeout = 30000, -- Timeout in milliseconds
+          timeout = 30000,
           extra_request_body = {
             temperature = 0.75,
             max_tokens = 16000
@@ -34,15 +31,6 @@ return {
             ["X-Title"] = "Avante.nvim"
           }
         },
-        -- openai = {
-        --   endpoint = "https://kivanov.msndr.net/v1",
-        --   model = "gpt-4o-mini",
-        --   timeout = 30000, -- Timeout in milliseconds
-        --     extra_request_body = {
-        --       temperature = 0.75,
-        --       max_tokens = 16000,
-        --     },
-        -- },
       },
       behaviour = {
         auto_approve_tool_permissions = false
@@ -51,34 +39,29 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "echasnovski/mini.pick",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "stevearc/dressing.nvim",
+      "folke/snacks.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
       {
-        -- support for image pasting
         "HakonHarnes/img-clip.nvim",
         event = "VeryLazy",
         opts = {
-          -- recommended settings
           default = {
             embed_image_as_base64 = false,
             prompt_for_file_name = false,
             drag_and_drop = {
               insert_mode = true,
             },
-            -- required for Windows users
             use_absolute_path = true,
           },
         },
       },
       {
-        -- Make sure to set this up properly if you have lazy=true
         'MeanderingProgrammer/render-markdown.nvim',
         opts = {
           file_types = { "markdown", "Avante" },
